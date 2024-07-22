1d35042feec2529f1e8fe02b4f6610a4bb6cf142
th1n-ker <vinkangnet@gmail.com>
2024-07-22 21:01:24 +0800

    feat(chore): get redirect url and redirect

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="chat-list-box">
+  <div class="login-box">
     <AppTitle />
     <LoginFrom />
     <EmailForm />
@@ -8,9 +8,23 @@
 <script setup lang="ts">
 import AppTitle from "@/components/common/AppTitle.vue";
 import { EmailForm, LoginFrom } from "./components";
+import { useUserStore } from "@/stores/user";
+const router = useRouter();
+const userStore = useUserStore();
+onMounted(() => {
+  const url = router.currentRoute.value.query.redirect;
+  let redirectUrl;
+  if (url) {
+    redirectUrl = url.toString();
+  }
+  if (userStore.getUserToken) {
+    if (redirectUrl) window.location.href = redirectUrl;
+    else router.push({ name: "Chat" });
+  }
+});
 </script>
 <style lang="scss" scoped>
-.chat-list-box {
+.login-box {
   height: 100vh;
   width: 100%;
   background-image: url("https://cdn.pixabay.com/photo/2024/06/21/15/18/road-8844555_1280.jpg");
